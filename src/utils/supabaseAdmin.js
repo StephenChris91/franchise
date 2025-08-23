45eca25cf06298b4dd68b076b907c3132c7c856c
StephenChris91 <StephenChris91@gmail.com>
2025-08-24 00:02:28 +0100

    FIX: fixed minor errors in frontned

--- src/utils/supabaseAdmin.js
+++ src/utils/supabaseAdmin.js
@@ -1,6 +1,6 @@
 // utils/supabaseAdmin.js
 import { createClient } from "@supabase/supabase-js";
-const SupabaseUrl = `${process.env.SUPABASE_URL}`;
+const SupabaseUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}`;
 const SupabaseAnonKey = `${process.env.SUPABASE_ANON_KEY}`;
 
 export const supabaseAdmin = createClient(SupabaseUrl, SupabaseAnonKey);
